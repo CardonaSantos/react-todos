63a5607738d9f94419e118c162bde2397e720fcc
Cardona Santos Miguel <106097059+CardonaSantos@users.noreply.github.com>
2022-10-04 15:29:33 -0600

    Updates

---
+++ precache-manifest.48b7b2bae87466461bf4bf4daef77edb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69195323d3a336796193",
+    "url": "/react-todos/static/css/main.2f4abcad.chunk.css"
+  },
+  {
+    "revision": "69195323d3a336796193",
+    "url": "/react-todos/static/js/main.69195323.chunk.js"
+  },
+  {
+    "revision": "9f71ac1ffdf39108d3f4",
+    "url": "/react-todos/static/css/1.cf5cefc5.chunk.css"
+  },
+  {
+    "revision": "9f71ac1ffdf39108d3f4",
+    "url": "/react-todos/static/js/1.9f71ac1f.chunk.js"
+  },
+  {
+    "revision": "23b2d069d95a08146c09",
+    "url": "/react-todos/static/js/runtime~main.23b2d069.js"
+  },
+  {
+    "revision": "7e47bc36d51980b7f5eea5d2980a05d4",
+    "url": "/react-todos/index.html"
+  }
+];
